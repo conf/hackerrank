4b4d366fe5d6cd7abbe39cc06e01a5a3d1bd684d
Alexey Shein <confik@gmail.com>
2015-01-15 12:35:43 +0500

    Add SolveMeSecond.

---
+++ lib/solve_me_second.rb
@@ -0,0 +1,28 @@
+require 'forwardable'
+class SolveMeSecond
+  extend Forwardable
+
+  delegate gets: :input, puts: :output
+  attr_reader :input, :output
+
+  def initialize(input = STDIN, output = STDOUT)
+    @input = input
+    @output = output
+  end
+
+  def main
+    read
+    puts solve
+  end
+
+  def read
+    @test_cases = gets.to_i
+    @arr = @test_cases.times.map { gets.split(' ').map(&:to_i) }
+  end
+
+  def solve
+    @arr.map { |numbers| numbers.reduce(&:+) }
+  end
+end
+
+SolveMeSecond.new.main unless ENV['TEST']
---
+++ spec/lib/solve_me_second_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'lib/solve_me_second'
+
+describe SolveMeSecond do
+  it_behaves_like 'from_fixtures'
+end
